8ab82085259d63f5daf740c6e3abf7dca6a55eff
Diogo Asenjo <diogoasenjo@outlook.com>
2023-06-04 09:25:19 -0300

    Exercicio 03

--- script.js
+++ script.js
@@ -67,6 +67,37 @@ for (let i = 0; i < listaA.length; i++) {
 }
 
 console.log(listaC);
+
 /*3) A ModalGR possui um sistema em desenvolvimento e numa parte específica será necessário separar dados numéricos de strings, a partir de uma variável do tipo string contendo diversos valores delimitados por vírgula, como por exemplo:
 Ana, 89,78, Maria, 45.8, 27, 56, Paula Pereira, 978, A, VIVA, 35, 125, 8999, ...Visando atender esse quesito, você foi escolhido para nos ajudar nessa solução! Após a separação dos dados exibi-los em tela.
 Observação:  O tamanho da string com os dados base é variável, ou seja, não possui um limite fixo.*/
+
+let listaDeDados = [
+  "Ana",
+  89,
+  78,
+  "Maria",
+  45.8,
+  27,
+  56,
+  "Paula Pereira",
+  978,
+  "A",
+  "VIVA",
+  35,
+  125,
+  8999,
+];
+let listaNums = [];
+let listaString = [];
+
+for (let i = 0; i < listaDeDados.length; i++) {
+  if (typeof listaDeDados[i] === "number") {
+    listaNums.push(listaDeDados[i]);
+  } else {
+    listaString.push(listaDeDados[i]);
+  }
+}
+
+console.log(listaNums);
+console.log(listaString);
